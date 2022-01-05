8c6d583d8367a133891e464e4cc6d6d7a809c5c9
Erik Arvstedt <erik.arvstedt@gmail.com>
2022-01-05 15:33:54 +0100

    set host key

--- ssh-container.nix
+++ ssh-container.nix
@@ -3,7 +3,20 @@
     extra.addressPrefix = "10.100.0";
     config = { pkgs, config, lib, ... }: {
       documentation.enable = false;
-      services.openssh.enable = true;
+      services.openssh = {
+        enable = true;
+        # Disable systemd socket activation
+        startWhenNeeded = false;
+        # Set host key
+        hostKeys = lib.mkForce [];
+        extraConfig = ''
+          HostKey /etc/ssh/host-key
+        '';
+      };
+      environment.etc."ssh/host-key" = {
+        text = builtins.readFile ./key;
+        mode = "400";
+      };
       users.users.root.openssh.authorizedKeys.keyFiles = [ ./key.pub ];
     };
   };
