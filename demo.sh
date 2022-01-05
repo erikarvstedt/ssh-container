b56074d10ee7a90781d55ec6a244755f10ffd299
Erik Arvstedt <erik.arvstedt@gmail.com>
2022-01-05 15:36:18 +0100

    container: set nixpkgs rev

--- demo.sh
+++ demo.sh
@@ -1,5 +1,8 @@
+set -euo pipefail
+
 cd "${BASH_SOURCE[0]%/*}"
+
 ./start.sh
 echo "connecting..."
-ssh root@ssh-server -o IdentityFile=key UserKnownHostsFile=known-hosts hostname
+ssh root@ssh-server -o IdentityFile=key -o UserKnownHostsFile=known-hosts hostname
 extra-container destroy ssh-server
--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 cd "${BASH_SOURCE[0]%/*}"
-extra-container create --start ./ssh-container.nix
+extra-container create --start --nixpkgs-path "fetchTarball https://github.com/NixOS/nixpkgs/archive/670c11d0da40c38f5d84a1cde13b52ff4d55b3bb.tar.gz" ./ssh-container.nix
